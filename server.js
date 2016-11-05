c5346f3b013b7534a491287cc0f08a1ee1b78a9f
Taylor Milliman <twm013@bucknell.edu>
2016-11-05 04:09:15 +0000

    setting up server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-app.get('/',function(req, res){
+app.get('/:input',function(req, res){
     var input = req.params.input;
     console.log(input);
     res.send("Hello World");
