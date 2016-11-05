4da6398d3f41a5f3771d97be806b5d4a9a2b5699
Taylor Milliman <twm013@bucknell.edu>
2016-11-05 17:03:54 +0000

    setting up with heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 
 var app = express();
+
+app.set('port', (process.env.PORT) || 8080);
+
 app.get('/:input',function(req, res){
     var input = req.params.input;
     var unix;
@@ -38,6 +41,6 @@ app.get('/', function(req,res){
        }); 
 });
 
-app.listen(8080, function(){
-    console.log("Listening on port 8080");
+app.listen(app.get('port'), function(){
+    console.log("Listening on port ", app.get('port'));
 });
